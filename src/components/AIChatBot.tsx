af4f9c134e0217a5a91c1bfb7ed210d97e736669
Ahmd Abdo <rehlathadaf@gmail.com>
2025-08-20 23:39:36 +0300

    Update AIChatBot.tsx

--- src/components/AIChatBot.tsx
+++ src/components/AIChatBot.tsx
@@ -246,7 +246,7 @@ export default function AIChatBot() {
                                      rounded-lg transition-all border border-green-500/50"
                         >
                           <MessageSquare className="w-3 h-3"/>
-                          أكمل عبر الواتساب
+                          كلم البائع عبر الواتساب
                         </a>
                       )}
                     </div>
